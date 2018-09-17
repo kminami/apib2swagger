2eac18a631c7f3fb0af69a2985978d40d7b006ae
Keisuke Minami <4717846+kminami@users.noreply.github.com>
2018-09-17 10:42:19 +0900

    Improve mson_to_json_schema

--- src/mson_to_json_schema.js
+++ src/mson_to_json_schema.js
@@ -3,24 +3,33 @@ var escapeJSONPointer = require('./escape_json_pointer');
 function convertMsonToJsonSchema(content) {
     // for apib._version = "4.0"
     var mson = content.content[0];
-    if (mson.element === 'array') {
-        if (!mson.content || mson.content.length === 0) {
-            return {type: 'array', items: {}};
-        } else if (mson.content.length === 1) {
-            return {type: 'array', items: {'$ref': '#/definitions/' + escapeJSONPointer(mson.content[0].element)}};
-        } else if (mson.content.length > 1) {
-            var items = [];
-            for (var i = 0; i < mson.content.length; i++) {
-                items.push({'$ref': '#/definitions/' + escapeJSONPointer(mson.content[i].element)});
+    return convert(mson);
+}
+
+function convert(mson) {
+    // mson.element = "boolean", "string", "number", "array", "enum", "object", CustomType
+    switch (mson.element) {
+        case 'array':
+            if (!mson.content || mson.content.length === 0) {
+                return { type: 'array', items: {} };
+            } else if (mson.content.length === 1) {
+                return { type: 'array', items: convert(mson.content[0]) };
+            } else if (mson.content.length > 1) {
+                return { type: 'array', items: { 'anyOf': mson.content.map(convert) } };
             }
-            return {type: 'array', items: {'anyOf': items}};
-        }
-    }
-    if (mson.element === 'enum') {
-        return convertEnum(mson.content);
-    }
-    if (mson.element !== 'object' && !mson.content) {
-        return {'$ref': '#/definitions/' + escapeJSONPointer(mson.element)};
+        case 'enum':
+            return convertEnum(mson.content);
+        case 'object':
+            break;
+        case 'boolean':
+        case 'string':
+        case 'number':
+            return { type: mson.element };
+        default:
+            if (!mson.content) {
+                return { '$ref': '#/definitions/' + escapeJSONPointer(mson.element) };
+            }
+            break;
     }
     // object
     var schema = {};
@@ -31,23 +40,12 @@ function convertMsonToJsonSchema(content) {
         var member = mson.content[j];
         if (member.element !== "member") continue;
         // MEMO: member.meta.description
-        if (member.content.value.element === 'array') {
-            // TODO: use member.content.value.content (schema for items)
-            schema.properties[member.content.key.content] = {type: 'array', items: {}};
-        } else if (member.content.value.element === 'enum') {
-            var body = {type: member.content.key.element};
-            body.enum = member.content.value.content.map(function(a) {
-              return a.content;
-            });
-            schema.properties[member.content.key.content] = body;
-        } else {
-            schema.properties[member.content.key.content] = {type: member.content.value.element};
-        }
+        schema.properties[member.content.key.content] = convert(member.content.value);
         if (!member.attributes || !member.attributes.typeAttributes) continue;
         for (var k = 0; k < member.attributes.typeAttributes.length; k++) {
             if (member.attributes.typeAttributes[k] === "fixedType") {
                 // handle when we have attributes containing objects
-                if (member.content.value.element === 'array'){
+                if (member.content.value.element === 'array') {
                     schema.properties[member.content.key.content] = {
                         'type': "array",
                         'items': {
@@ -55,7 +53,7 @@ function convertMsonToJsonSchema(content) {
                         }
                     };
                 } else {
-                    schema.properties[member.content.key.content] = {'$ref': '#/definitions/' + escapeJSONPointer(member.content.value.element)};
+                    schema.properties[member.content.key.content] = { '$ref': '#/definitions/' + escapeJSONPointer(member.content.value.element) };
                 }
             }
             if (member.attributes.typeAttributes[k] === "required") {
@@ -70,14 +68,14 @@ function convertMsonToJsonSchema(content) {
     }
 
     if (mson.element !== 'object') {
-        return {'allOf': [{'$ref':'#/definitions/' + escapeJSONPointer(mson.element)}, schema]};
+        return { 'allOf': [{ '$ref': '#/definitions/' + escapeJSONPointer(mson.element) }, schema] };
     }
 
     return schema;
 }
 
 function convertEnum(contents) {
-    var schema = {type: '', enum: []};
+    var schema = { type: '', enum: [] };
     for (var i = 0; i < contents.length; i++) {
         var content = contents[i];
         if (!schema.type) {
